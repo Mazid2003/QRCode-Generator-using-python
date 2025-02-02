6f1df15c78aa7e6aad66e63542d70d83795f5c4d
Mohammad Mazid <102860379+Mazid2003@users.noreply.github.com>
2025-02-02 08:08:30 +0530

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -1,5 +1,7 @@
-import qrcode
-data="http://mazidmohammad.pythonanywhere.com"
-qr=qrcode.make(data)
+import qrcode  #necessary package for qrcode generation 
+data="http://mazidmohammad.pythonanywhere.com"   #assign any website link to a variable 
+qr=qrcode.make(data)  
+#make a methode in qrcode package which is used to create the qrcode 
 qr.save("C:\\Users\\USER\\Desktop\\final project\\mazidatlas.png")
+#save is a method which is used to store the qrcode for the given website
 print("QR successfull")
